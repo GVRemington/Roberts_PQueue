d092c3c37199f02fb8cf3c33446508b200eea7cf
GVRemington <104083156+GVRemington@users.noreply.github.com>
2022-10-16 19:24:46 -0700

    Add project files.

---
+++ Roberts_PQueue/Emergency_Room.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roberts_PQueue
+{
+    internal class Emergency_Room
+    {
+
+
+        private PriorityQueue<string, int> pQueue;
+        int count = 0;
+        private string _name;
+
+        public Emergency_Room()
+        {
+            pQueue = new PriorityQueue<string, int>();
+          
+        }
+
+        public void addPatient(string name, int priority) //enqueue method
+        {
+            pQueue.Enqueue(name, priority);
+            count = pQueue.Count;
+        }
+
+        public string testFiFo() //dequeue and testor method
+        {
+            _name = pQueue.Dequeue();
+            return _name;
+        }
+
+        public string[] patientList()
+        {
+            string[] patientList = new string[pQueue.Count];
+            int count = pQueue.Count;
+            for (int i = 0; i < count; i++)
+            {
+               
+                testFiFo();
+                patientList[i] = _name;
+                
+            }
+            foreach (string patient in patientList)
+            {
+                int i = 1;
+                _name = patient;
+                addPatient(_name, i);
+                i++;
+                count = pQueue.Count;
+              
+            }
+            
+            //Console.WriteLine("\n this counts the final pQueue to verify 5 elements went back in => " + count + " <= yep!\n");
+            
+            return patientList;
+        }
+      
+      
+
+
+
+
+
+
+
+    }
+
+}
---
+++ Roberts_PQueue/Program.cs
@@ -0,0 +1,82 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Roberts_PQueue;
+//Rayleen Roberts
+//IT 113
+//Notes: My first test with the hard codes comes out correctly all around
+//       However, when I start using user input, they do not go according to plan
+//       For example: if priority 1 is selected, the new patient will be placed
+//                    between Fred Flintstone and Johny Quest. If I add 2 number 1s
+//                    they will go between AND flipped backward.
+//       If however, priority selection is 4 or higher,
+//                   they all go in and come out correctly haha!!
+//       I think that the problem is in my loop logic, not sure how, and using the 
+//       debugger on a menu loop is proving to be daunting.
+//Behaviors Not Implemented:
+//       None that I know of....albeit my menu and menu loop logic are a bit buggy.
+//                               
+
+
+
+
+Emergency_Room ER = new Emergency_Room();
+
+ER.addPatient("Mickey Mouse", 2);
+ER.addPatient("Fred FlintStone", 1);
+ER.addPatient("George Jetson", 2);
+ER.addPatient("Barney Rubble", 3);
+ER.addPatient("Johny Quest", 1);
+
+//test print
+Console.WriteLine("\n");
+foreach (string patient in ER.patientList())
+{
+    Console.WriteLine(patient);
+}
+Console.WriteLine("\n If requeue was done correctly, Fred Flintstone should appear here => " + ER.testFiFo() + " <= success!");
+//end of test print
+
+
+
+Console.Write("\n\tTo Add New Patient Select 1...to List all Patients Select 2...if you wish to exit Select 3\t");
+int selection = int.Parse(Console.ReadLine());
+
+while (selection != 3)
+{
+    if (selection == 1)
+    {
+        Console.Write("\tPlease enter the patient's Full name\t\t");
+        string newPat = Console.ReadLine();
+        Console.Write("\tPlease Enter Priority Level\t\t");
+        int priority = int.Parse(Console.ReadLine());
+
+        ER.addPatient(newPat, priority);
+
+        Console.Write("\n\tTo Add New Patient Select 1...to List all Patients Select 2...if you wish to exit Select 3\t");
+        selection = int.Parse(Console.ReadLine());
+
+    }
+    if (selection == 2)
+    {
+        Console.WriteLine("\n");
+        foreach (string patient in ER.patientList())
+        {
+            Console.WriteLine(patient);
+        }
+        Console.Write("\n\tTo Add New Patient Select 1...to List all Patients Select 2...if you wish to exit Select 3\t");
+        selection = int.Parse(Console.ReadLine());
+
+    }
+}
+if (selection == 3)
+{
+    Console.WriteLine("\n\tThis verifies that requeue was correct.  Fred Flintstone should appear here => " + ER.testFiFo() + " <= success!");
+    Console.WriteLine("\n\tGood Bye");
+}
+
+
+
+
+
+
+
+
